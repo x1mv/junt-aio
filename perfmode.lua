539860a6aae6b46e1d42150d22ebaaec25b0a206
cykahvh <103964666+x1mv@users.noreply.github.com>
2022-12-21 23:43:02 +0100

    Create perfmode.lua

---
+++ perfmode.lua
@@ -0,0 +1,104 @@
+setfpscap(3)
+game:service"RunService":SetRobloxGuiFocused(false)
+local FuckniggerS = Instance.new("ScreenGui")
+local Curbstompanigger = Instance.new("Frame")
+local Gradientnigger = Instance.new("UIGradient")
+local rapeniggers = Instance.new("TextLabel")
+local rapedahoodmods = Instance.new("TextLabel")
+local niggerstomper = Instance.new("TextButton")
+local UIGradient = Instance.new("UIGradient")
+local UICorner = Instance.new("UICorner")
+local rapeniggers2 = Instance.new("TextLabel")
+local rapedahoodmods2 = Instance.new("TextLabel")
+
+--Properties:
+
+FuckniggerS.Name = "Fuck niggerS"
+FuckniggerS.Parent = game.CoreGui
+FuckniggerS.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Curbstompanigger.Name = "Curb stomp a nigger"
+Curbstompanigger.Parent = FuckniggerS
+Curbstompanigger.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Curbstompanigger.Position = UDim2.new(-0.155110762, 0, -0.123441398, 0)
+Curbstompanigger.Size = UDim2.new(98.2623291, 0, 98.3466339, 0)
+
+Gradientnigger.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 170, 0)), ColorSequenceKeypoint.new(0.01, Color3.fromRGB(0, 0, 255)), ColorSequenceKeypoint.new(0.01, Color3.fromRGB(255, 32, 3)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(128, 0, 255))}
+Gradientnigger.Name = "Gradient nigger"
+Gradientnigger.Parent = Curbstompanigger
+
+rapeniggers.Name = "rape niggers"
+rapeniggers.Parent = FuckniggerS
+rapeniggers.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+rapeniggers.BackgroundTransparency = 1.000
+rapeniggers.Position = UDim2.new(0.339528233, 0, 0, 0)
+rapeniggers.Size = UDim2.new(0.320228726, 0, 0.231920198, 0)
+rapeniggers.Font = Enum.Font.AmaticSC
+rapeniggers.Text = "Junt Alt Control"
+rapeniggers.TextColor3 = Color3.fromRGB(46, 46, 46)
+rapeniggers.TextScaled = true
+rapeniggers.TextSize = 60.000
+rapeniggers.TextWrapped = true
+
+rapedahoodmods.Name = "rape da hood mods"
+rapedahoodmods.Parent = FuckniggerS
+rapedahoodmods.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+rapedahoodmods.BackgroundTransparency = 1.000
+rapedahoodmods.Position = UDim2.new(0.376299262, 0, 0.168329179, 0)
+rapedahoodmods.Size = UDim2.new(0.247717872, 0, 0.0386533663, 0)
+rapedahoodmods.Font = Enum.Font.Merriweather
+rapedahoodmods.Text = "Performance Mode"
+rapedahoodmods.TextColor3 = Color3.fromRGB(46, 46, 46)
+rapedahoodmods.TextScaled = true
+rapedahoodmods.TextSize = 60.000
+rapedahoodmods.TextWrapped = true
+
+niggerstomper.Name = "niggerstomper"
+niggerstomper.Parent = FuckniggerS
+niggerstomper.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+niggerstomper.Position = UDim2.new(0.33523947, 0, 0.673316717, 0)
+niggerstomper.Size = UDim2.new(0.324517548, 0, 0.140897751, 0)
+niggerstomper.Font = Enum.Font.Gotham
+niggerstomper.Text = "Stop Perf. Mode"
+niggerstomper.TextColor3 = Color3.fromRGB(0, 0, 0)
+niggerstomper.TextScaled = true
+niggerstomper.TextSize = 14.000
+niggerstomper.TextWrapped = true
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 5, 5)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(67, 158, 10))}
+UIGradient.Parent = niggerstomper
+
+UICorner.CornerRadius = UDim.new(0.100000001, 5)
+UICorner.Parent = niggerstomper
+
+rapeniggers2.Name = "rape niggers2"
+rapeniggers2.Parent = FuckniggerS
+rapeniggers2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+rapeniggers2.BackgroundTransparency = 1.000
+rapeniggers2.Position = UDim2.new(0.33523947, 0, 0, 0)
+rapeniggers2.Size = UDim2.new(0.320228726, 0, 0.231920198, 0)
+rapeniggers2.Font = Enum.Font.AmaticSC
+rapeniggers2.Text = "Junt Alt Control"
+rapeniggers2.TextColor3 = Color3.fromRGB(232, 232, 232)
+rapeniggers2.TextScaled = true
+rapeniggers2.TextSize = 60.000
+rapeniggers2.TextWrapped = true
+
+rapedahoodmods2.Name = "rape da hood mods2"
+rapedahoodmods2.Parent = FuckniggerS
+rapedahoodmods2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+rapedahoodmods2.BackgroundTransparency = 1.000
+rapedahoodmods2.Position = UDim2.new(0.373838484, 0, 0.168329179, 0)
+rapedahoodmods2.Size = UDim2.new(0.247717872, 0, 0.0386533663, 0)
+rapedahoodmods2.Font = Enum.Font.Merriweather
+rapedahoodmods2.Text = "Performance Mode"
+rapedahoodmods2.TextColor3 = Color3.fromRGB(216, 216, 216)
+rapedahoodmods2.TextScaled = true
+rapedahoodmods2.TextSize = 60.000
+rapedahoodmods2.TextWrapped = true
+niggerstomper.MouseButton1Down:Connect(function()
+    print'Fuck you niggerstomper'
+    game.CoreGui['Fuck niggerS']:Destroy()
+    setfpscap(999)
+    game:service"RunService":SetRobloxGuiFocused(false)
+end)
